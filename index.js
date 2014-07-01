2b1d28200161f0d6a882766eda45310621532260
Mike Bostock <mbostock@gmail.com>
2014-07-01 12:37:07 -0400

    Add writer.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ exports.readSync = require("./lib/rw/read-sync");
 exports.write = require("./lib/rw/write");
 exports.writeSync = require("./lib/rw/write-sync");
 
+exports.writer = require("./lib/rw/writer");
 exports.reader = require("./lib/rw/reader");
 exports.bufferReader = require("./lib/rw/buffer-reader");
 exports.lineReader = require("./lib/rw/line-reader");

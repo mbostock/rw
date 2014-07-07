1e6cb90fd21ce78ca576baf6113675cbd9030f1f
Mike Bostock <mbostock@gmail.com>
2014-07-07 15:26:43 -0700

    Generic reader & writer.
    Rename file-specific functions.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-exports.read = require("./lib/rw/read");
-exports.readSync = require("./lib/rw/read-sync");
-exports.write = require("./lib/rw/write");
-exports.writeSync = require("./lib/rw/write-sync");
-
-exports.writer = require("./lib/rw/writer");
-exports.lineWriter = require("./lib/rw/line-writer");
-
 exports.reader = require("./lib/rw/reader");
-exports.lineReader = require("./lib/rw/line-reader");
-exports.fixedReader = require("./lib/rw/fixed-reader");
+exports.writer = require("./lib/rw/writer");
+
+exports.fileReader = require("./lib/rw/file-reader");
+exports.fileWriter = require("./lib/rw/file-writer");
+
+exports.readFile = require("./lib/rw/read-file");
+exports.readFileSync = require("./lib/rw/read-file-sync");
+exports.writeFile = require("./lib/rw/write-file");
+exports.writeFileSync = require("./lib/rw/write-file-sync");
